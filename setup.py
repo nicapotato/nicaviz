92d40793f5479d8a9d2805287c70437f06ac5d5b
Nick Brooks <nbrooks@mycwt.com>
2020-12-18 16:59:26 +0000

    build basic structure I

--- setup.py
+++ setup.py
@@ -4,19 +4,19 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="nicapotato", # Replace with your own username
+    name="nicaviz", # Replace with your own username
     version="0.0.1",
     author="nicapotato",
     author_email="nick.brooks27@gmail.com",
     description="Python Visualization Package",
     long_description="Build over seaborn and matplotlib",
     long_description_content_type="text/markdown",
-    url="https://github.com/nicapotato/nica",
+    url="https://github.com/nicapotato/nicaviz",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.5',
 )
