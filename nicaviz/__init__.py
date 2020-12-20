f5fc596ae882ad9d69c2c57f517bdf87ca9333c4
Nick Brooks <nbrooks@mycwt.com>
2020-12-20 23:03:00 +0000

    Fix folder structure

---
+++ nicaviz/__init__.py
@@ -0,0 +1,2 @@
+import plot
+# from .processsing import *
---
+++ nicaviz/plot/__init__.py
@@ -0,0 +1,4 @@
+from .categoricalplots import big_bar_cloud
+from .languageplots import big_word_cloud
+from .continuousplot import big_boxplotter
+from .describe import describe
