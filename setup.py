1eb3552beaaa64b2fb7e50ce38e434ca4bcec5bf
Nick Brooks <nbrooks@mycwt.com>
2021-09-27 11:48:50 +0100

    v0.1.0 - Add Python and Pandas Utils (memory and timer)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="nicaviz",
-    version="0.0.9",
+    version="0.1.0",
     author="nicapotato",
     author_email="nick.brooks27@gmail.com",
     description="Python Visualization Package",
